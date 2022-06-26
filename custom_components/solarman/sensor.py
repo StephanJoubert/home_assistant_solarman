4165b37b757fdf48e1f6d72c4dea0aaae52841f8
Austin Mroczek <austin@mroczek.org>
2022-06-25 18:00:37 -0700

    update docstrings and run black on sensor

--- custom_components/solarman/sensor.py
+++ custom_components/solarman/sensor.py
@@ -4,9 +4,12 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.components.sensor import SensorEntity
+from .const import DOMAIN
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+):
     """Setup from the config entry."""
     inverter = hass.data[DOMAIN][entry.entry_id].inverter
 
@@ -21,38 +24,39 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     hass_sensors.append(SolarmanStatus(inverter, "status_connection"))
 
     async_add_entities(hass_sensors)
-    
+
 
 class SolarmanStatus(SensorEntity):
+    """Generic Solarman sensor entity."""
+
     def __init__(self, inverter, field_name):
+        """Initialize."""
         self.inverter = inverter
         self._field_name = field_name
         self._inverter_name = inverter.name
         self._serial_number = inverter.serial_number
-        self._attr_icon = 'mdi:magnify'
+        self._attr_icon = "mdi:magnify"
         self._attr_name = "{} {}".format(self._inverter_name, field_name)
-        self._attr_unique_id = "{}_{}_{}".format(self._inverter_name, self._serial_number, field_name)
-
+        self._attr_unique_id = "{}_{}_{}".format(
+            self._inverter_name, self._serial_number, field_name
+        )
 
     def update(self):
+        """Update this sensor using the data."""
         self._attr_state = getattr(self.inverter, self._field_name)
 
-#############################################################################################################
-#  Entity displaying a text field read from the inverter
-#   Overrides the Status entity, supply the configured icon, and updates the inverter parameters
-#############################################################################################################
 
 class SolarmanSensorText(SolarmanStatus):
-    def __init__(self, inverter, sensor):
-        SolarmanStatus.__init__(self, inverter, sensor['name'])
-        if 'icon' in sensor:
-            self._attr_icon = sensor['icon']
+    """Entity displaying a text field read from the inverter."""
 
+    def __init__(self, inverter, sensor):
+        """Initialize."""
+        SolarmanStatus.__init__(self, inverter, sensor["name"])
+        if "icon" in sensor:
+            self._attr_icon = sensor["icon"]
 
     def update(self):
-    #  Update this sensor using the data.
-    #  Get the latest data and use it to update our sensor state.
-    #  Retrieve the sensor data from actual interface
+        """Update this sensor using the data."""
         self.inverter.update()
 
         val = self.inverter.get_current_val()
@@ -61,19 +65,15 @@ class SolarmanSensorText(SolarmanStatus):
                 self._attr_state = val[self._field_name]
 
 
-#############################################################################################################
-#  Entity displaying a numeric field read from the inverter
-#   Overrides the Text sensor and supply the device class, last_reset and unit of measurement
-#############################################################################################################
-
-
 class SolarmanSensor(SolarmanSensorText):
+    """Entity displaying a numeric field read from the inverter."""
+
     def __init__(self, inverter_name, inverter, sensor):
+        """Initialize."""
         SolarmanSensorText.__init__(self, inverter_name, inverter, sensor)
-        self._attr_device_class = sensor['class']
-        if 'state_class' in sensor:
-            self._attr_state_class = sensor['state_class']
+        self._attr_device_class = sensor["class"]
+        if "state_class" in sensor:
+            self._attr_state_class = sensor["state_class"]
 
-        self._attr_native_unit_of_measurement = sensor['uom']
+        self._attr_native_unit_of_measurement = sensor["uom"]
         return
-
