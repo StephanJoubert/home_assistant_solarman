720bd3c6e571153f344d6cbd159bf57378dfb3da
Kolbi <MatthiasKolb@2-wheel-planet.de>
2022-11-25 10:33:34 +0100

    Update const.py

--- custom_components/solarman/const.py
+++ custom_components/solarman/const.py
@@ -5,7 +5,7 @@ DOMAIN = 'solarman'
 DEFAULT_PORT_INVERTER = 8899
 DEFAULT_INVERTER_MB_SLAVEID = 1
 DEFAULT_LOOKUP_FILE = 'deye_hybrid.yaml'
-LOOKUP_FILES = ['deye_hybrid.yaml', 'deye_string.yaml', 'sofar_lsw3.yaml', 'sofar_wifikit.yaml', 'solis_hybrid.yaml', 'sofar_g3hyd.yaml', 'custom_parameters.yaml']
+LOOKUP_FILES = ['deye_4mppt.yaml', 'deye_hybrid.yaml', 'deye_string.yaml', 'sofar_lsw3.yaml', 'sofar_wifikit.yaml', 'solis_hybrid.yaml', 'sofar_g3hyd.yaml', 'custom_parameters.yaml']
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=30)
 
